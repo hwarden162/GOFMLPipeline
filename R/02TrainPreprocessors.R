815a8509f454c530e56e0000262e86f6e7c56297
Hugh Warden <hugh.warden@outlook.com>
2025-05-15 21:22:33 +0100

    Train preprocessors

---
+++ R/02TrainPreprocessors.R
@@ -0,0 +1,30 @@
+library(tidymodels)
+library(tidyverse)
+
+full_data_train <- read_csv("./data/full_data_train.csv")
+area_data_train <- read_csv("./data/area_data_train.csv")
+spatial_data_train <- read_csv("./data/spatial_data_train.csv")
+
+full_data_recipe <- recipe(GOF ~ ., data = full_data_train) |> 
+  step_zv(all_numeric_predictors()) |> 
+  step_nzv(all_numeric_predictors()) |> 
+  prep()
+
+area_data_recipe <- recipe(GOF ~ ., data = area_data_train) |> 
+  step_zv(all_numeric_predictors()) |> 
+  step_nzv(all_numeric_predictors()) |> 
+  prep()
+
+spatial_data_recipe <- recipe(GOF ~ ., data = spatial_data_train) |> 
+  step_zv(all_numeric_predictors()) |> 
+  step_nzv(all_numeric_predictors()) |> 
+  prep()
+
+full_data_recipe |> 
+  saveRDS("./models/full_data_recipe.rds")
+
+area_data_recipe |> 
+  saveRDS("./models/area_data_recipe.rds")
+
+spatial_data_recipe |> 
+  saveRDS("./models/spatial_data_recipe.rds")
