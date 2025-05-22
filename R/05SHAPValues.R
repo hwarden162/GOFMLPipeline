0f249f8544076ffa1987936e8a04bcd05e4aa532
Hugh Warden <hugh.warden@outlook.com>
2025-05-22 16:26:47 +0100

    Update SHAP code

--- R/05SHAPValues.R
+++ R/05SHAPValues.R
@@ -25,26 +25,33 @@ spatial_data_model <- readRDS("./models/spatial_data_model.rds")
 
 get_shap_vals <- function(data_recipe, data_train, data_test, data_model) {
   
-  X <- data_test |> 
+  X_test <- data_test |> 
+    select(-GOF) |> 
+    as.matrix()
+  
+  X_train <- data_train |> 
     select(-GOF) |> 
     as.matrix()
   
   predict_fn <- function(object, newdata) {
-    preds <- predict(object, new_data = newdata, type = "prob")
+    preds <- predict(object, new_data = newdata |> as_tibble(), type = "prob")
     dplyr::pull(preds, ".pred_Kras")
   }
   
-  vals <- fastshap::explain(
-    object = data_model,
-    X = X,
-    pred_wrapper = predict_fn,
-    newdata = X,
-    nsim = 1000
-  )
-  
   baseline <- mean(predict_fn(data_model, newdata = data_train))
   
-  shapviz(vals, X=X, baseline=baseline)
+  vals <- fastshap::explain(
+    object = data_model,
+    feature_names = X_test |> colnames(),
+    X = X_train,
+    nsim = 1000,
+    pred_wrapper = predict_fn,
+    newdata = X_test,
+    baseline = baseline,
+    parallel = TRUE
+  )
+  
+  shapviz(vals, X=X_test, baseline=baseline)
 }
 
 full_data_vals <- get_shap_vals(full_data_recipe, full_data_train, full_data_test, full_data_model)
